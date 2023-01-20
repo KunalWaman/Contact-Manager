da40710e994efa25a082c1d78ff75251c026d3d5
KunalWaman <kunalwaman04@gmail.com>
2023-01-21 00:09:50 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,42 @@ import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import Contactlist from "./Contactlist";
+import Contactcard from './Contactcard';
 
 function App() {
+  const contacts =[
+    {
+      id:"1",
+      name:"omkar",
+      email:"omkar@gmail.com",
+    },
+    {
+      id:"2",
+      name:"om",
+      email:"om@gmail.com",
+    }
+  ]
+  // const recent =[
+  //   {
+  //    id:"*",
+  //    name :"v",
+  //    email:"v@gmail.com",
+  //   },
+  //   {
+  //     id:"*1",
+  //     name :"s",
+  //     email:"s@gmail.com",
+  //    }
+  // ]
+
+
+    {/* //recent ={recent} */}
   return (
-  <div>
+  <div className="ui container" >
     <Header />
     <AddContact/>
-    <Contactlist /> 
+    <Contactlist contacts= {contacts}  />
+     <Contactcard />
   </div>
   );
 }
--- src/components/Contactlist.js
+++ src/components/Contactlist.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Contactcard from "./Contactcard";
 
-const Contactlist =() =>{
+const Contactlist =(props) =>{
+    console.log(props);
+
+    const renderContactlist = props.contacts.map((contact) =>{
+        return (
+          <Contactcard contact={contact} /> 
+        );
+    }
+    );
     return(
-        <div className="ui celled list">
-            Contact list
-        </div>
+        <div className="ui celled list"> {renderContactlist} </div>
+        
     );
 }
 
